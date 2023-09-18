52f443bfa7c1899da5f69c14eb2333763baf9bcc
GodPepe7 <privatalex@gmail.com>
2023-09-18 16:16:53 +0700

    Display hours forecast and add fahrenheit toggle

--- src/domController.js
+++ src/domController.js
@@ -4,15 +4,73 @@ import fetchWeatherData from "./fetchWeatherData";
 
 const domController = (() => {
   const locationInput = document.querySelector(".head__location-input");
-  const enterBtn = document.querySelector(".enter-btn");
+  const form = document.querySelector("#target-location-form");
   const overviewLocation = document.querySelector(".overview__location");
   const overviewTemp = document.querySelector(".overview__temp");
   const overviewDate = document.querySelector(".overview__date");
   const days = document.querySelectorAll(".day");
-  //   const hours = document.querySelectorAll(".hour");
+  const hoursBody = document.querySelector(".hours__body");
+  const hourTemplate = document.querySelector("#hour-template");
+  const tempToggleCheckbox = document.querySelector(".temp-toggle");
+
+  let isCelcius = true;
+
+  const createHourDivs = () => {
+    const fragment = document.createDocumentFragment();
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 24; i++) {
+      const hourDiv = hourTemplate.content.cloneNode(true);
+      fragment.append(hourDiv);
+    }
+    hoursBody.append(fragment);
+  };
+  createHourDivs();
+
+  const getDaysData = (weatherData) => {
+    const forecastData = weatherData.forecast.forecastday;
+    const daysData = forecastData.map((forecast) => {
+      const {
+        date,
+        day: {
+          maxtemp_c,
+          maxtemp_f,
+          mintemp_c,
+          mintemp_f,
+          daily_chance_of_rain,
+          condition: { text, icon },
+        },
+      } = forecast;
+      const minTemp = isCelcius ? `${mintemp_c}°C` : `${mintemp_f}°F`;
+      const maxTemp = isCelcius ? `${maxtemp_c}°C` : `${maxtemp_f}°F`;
+      return {
+        date,
+        minTemp,
+        maxTemp,
+        daily_chance_of_rain,
+        text,
+        icon,
+      };
+    });
+    return daysData;
+  };
+
+  const getHoursData = (weatherData) => {
+    const hours = weatherData.forecast.forecastday[0].hour;
+    const hoursData = hours.map((hour) => {
+      const {
+        time,
+        temp_c,
+        temp_f,
+        condition: { icon },
+      } = hour;
+      const temp = isCelcius ? `${temp_c}°C` : `${temp_f}°F`;
+      return { time, temp, icon };
+    });
+    return hoursData;
+  };
 
   const fillOverview = (overviewData) => {
-    const { name, country, localtime, temp_c } = overviewData;
+    const { name, country, localtime, temp } = overviewData;
     overviewLocation.textContent = `${name}, ${country}`;
     let formattedDate = new Date(localtime);
     const options = {
@@ -22,22 +80,13 @@ const domController = (() => {
       day: "numeric",
     };
     formattedDate = formattedDate.toLocaleDateString("en-US", options);
-    overviewTemp.textContent = `${temp_c}`;
+    overviewTemp.textContent = `${temp}`;
     overviewDate.textContent = `${formattedDate}`;
   };
 
   const fillDays = (daysData) => {
     daysData.forEach((day, index) => {
-      const {
-        date,
-        maxtemp_c,
-        // maxtemp_f,
-        mintemp_c,
-        // mintemp_f,
-        daily_chance_of_rain,
-        text,
-        icon,
-      } = day;
+      const { date, minTemp, maxTemp, daily_chance_of_rain, text, icon } = day;
       const dayDiv = days[index];
       const dayWeekdayP = dayDiv.querySelector(".day__date");
       const weekDay = new Date(date).toLocaleDateString("en-US", {
@@ -45,11 +94,11 @@ const domController = (() => {
       });
       dayWeekdayP.textContent = weekDay;
       const dayWeatherIcon = dayDiv.querySelector(".weather-icon");
-      dayWeatherIcon.src = icon;
+      dayWeatherIcon.src = `https:${icon}`;
       const dayMaxTempP = dayDiv.querySelector(".day__high");
-      dayMaxTempP.textContent = `${maxtemp_c}°C`;
+      dayMaxTempP.textContent = maxTemp;
       const dayMinTempP = dayDiv.querySelector(".day__low");
-      dayMinTempP.textContent = `${mintemp_c}°C`;
+      dayMinTempP.textContent = minTemp;
       const conditionP = dayDiv.querySelector(".condition-text");
       conditionP.textContent = text;
       const dayChanceOfRainP = dayDiv.querySelector(".rain-chance");
@@ -57,43 +106,56 @@ const domController = (() => {
     });
   };
 
-  const updateScreen = (weatherData) => {
-    const { name, region, country, localtime } = weatherData.location;
-    const { temp_c, temp_f } = weatherData.current;
-    const overviewData = { name, region, country, localtime, temp_c, temp_f };
-    fillOverview(overviewData);
-    const forecastData = weatherData.forecast.forecastday;
-    const daysData = forecastData.map((forecast) => {
-      const { date } = forecast;
-      const {
-        maxtemp_c,
-        maxtemp_f,
-        mintemp_c,
-        mintemp_f,
-        daily_chance_of_rain,
-      } = forecast.day;
-      const { text, icon } = forecast.day.condition;
-      return {
-        date,
-        maxtemp_c,
-        maxtemp_f,
-        mintemp_c,
-        mintemp_f,
-        daily_chance_of_rain,
-        text,
-        icon,
-      };
+  const hourDivs = document.querySelectorAll(".hour");
+  const fillHours = (hoursData) => {
+    hoursData.forEach((hour, index) => {
+      const { temp, icon, time } = hour;
+      const hourDiv = hourDivs[index];
+      const timeInHours = time.split(" ")[1];
+      hourDiv.querySelector(".hour__time").textContent = timeInHours;
+      hourDiv.querySelector(".weather-icon").src = `https:${icon}`;
+      hourDiv.querySelector(".hour__temp").textContent = temp;
     });
-    fillDays(daysData);
   };
 
-  const getWeather = async () => {
-    const query = locationInput.value;
+  const updateScreen = (weatherData) => {
+    const {
+      location: { name, region, country, localtime },
+      current: { temp_c, temp_f },
+    } = weatherData;
+    const temp = isCelcius ? `${temp_c}°C` : `${temp_f}°F`;
+    const overviewData = { name, region, country, localtime, temp };
+    fillOverview(overviewData);
+
+    const daysData = getDaysData(weatherData);
+    fillDays(daysData);
+
+    const hoursData = getHoursData(weatherData);
+    fillHours(hoursData);
+  };
+
+  const getWeather = async (query) => {
     const weatherData = await fetchWeatherData(query);
+    if (!weatherData) return;
+    tempToggleCheckbox.value = query;
     updateScreen(weatherData);
   };
 
-  enterBtn.addEventListener("click", getWeather);
+  const formHandler = (e) => {
+    e.preventDefault();
+    if (!form.checkValidity()) return;
+    const query = locationInput.value;
+    getWeather(query);
+  };
+  form.addEventListener("submit", formHandler);
+
+  function toggleTemp() {
+    isCelcius = !isCelcius;
+    getWeather(tempToggleCheckbox.value);
+  }
+  tempToggleCheckbox.addEventListener("click", toggleTemp);
+
+  getWeather("Dong Hoi");
 })();
 
 export default domController;
--- src/fetchWeatherData.js
+++ src/fetchWeatherData.js
@@ -2,9 +2,14 @@ const fetchWeatherData = async (location) => {
   if (!location) return null;
   const API_KEY = "5b7096ab9b684576ae630954231209";
   const URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3`;
-  const response = await fetch(URL, { mode: "cors" });
-  const weatherData = await response.json();
-  return weatherData;
+  try {
+    const response = await fetch(URL, { mode: "cors" });
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 };
 
 export default fetchWeatherData;
